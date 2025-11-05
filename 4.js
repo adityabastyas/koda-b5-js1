86f53fc0eca2442cd6f516e996e2f40c8e564cd0
aditya bastyas <adityabastyasmulya@gmail.com>
2025-11-05 20:06:48 +0700

    Minitask 1 "football"
    Membuat progam untuk menampilkan string "football" ketika mengakses array my[0].very["best"][1].sport.is

---
+++ 4.js
@@ -0,0 +1,5 @@
+// Membuat progam untuk menampilkan string "football" ketika mengakses array my[0].very["best"][1].sport.is
+
+const my = [{ very: { best: ['l', { sport: { is: 'football' } }] } }, 's'];
+
+console.log(my[0].very['best'][1].sport.is);
