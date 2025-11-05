ff5819873682b663d7c772e0518aa7acfc9d3e2b
aditya bastyas <adityabastyasmulya@gmail.com>
2025-11-05 20:03:25 +0700

    Minitask 1 who.am.i
    Membuat progam untuk menampilkan nama kalian berbentuk string ketika melakukan akses objek dengan who.am.i

---
+++ 1.js
@@ -0,0 +1,8 @@
+//Membuat progam untuk menampilkan nama kalian berbentuk string ketika melakukan akses objek dengan who.am.i
+
+const who = {
+  am: {
+    i: 'Aditya Bastyas Mulya',
+  },
+};
+console.log(who.am.i);
